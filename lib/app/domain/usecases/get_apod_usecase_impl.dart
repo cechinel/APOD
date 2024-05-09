0e62b9c00661ef85bfd95157713f9a8054767e86
cechinel <cechine88@gmail.com>
2024-05-09 20:54:52 -0300

    chore: update usecase to communicate with repository

--- lib/app/domain/usecases/get_apod_usecase_impl.dart
+++ lib/app/domain/usecases/get_apod_usecase_impl.dart
@@ -1,10 +1,14 @@
 import '../entities/apod_entity.dart';
+import '../repositories/get_apod_repository.dart';
 import './get_apod_usecase.dart';
 
 class GetApodUsecaseImpl implements GetApodUsecase {
+  final GetApodRepository repository;
+
+  GetApodUsecaseImpl(this.repository);
+
   @override
-  Future<ApodEntity> call() {
-    // TODO: implement call
-    throw UnimplementedError();
+  Future<ApodEntity> call() async {
+    return await repository();
   }
 }
