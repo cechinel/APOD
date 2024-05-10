21b6f794c817879002d92196a648556b4ae2e1ff
cechinel <cechine88@gmail.com>
2024-05-10 17:44:36 -0300

    test: update should throw an exception when request fails

--- test/app/domain/repositories/get_apod_repository_test.dart
+++ test/app/domain/repositories/get_apod_repository_test.dart
@@ -2,6 +2,7 @@ import 'package:apod/app/data/datasources/get_apod_datasource.dart';
 import 'package:apod/app/data/repositories/get_apod_repository_impl.dart';
 import 'package:apod/app/domain/entities/apod_entity.dart';
 import 'package:apod/app/domain/repositories/get_apod_repository.dart';
+import 'package:apod/services/exceptions/apod_generic_exception.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -31,8 +32,10 @@ void main() {
   });
 
   test('Should throw an exception when request fails', () async {
-    when(repository()).thenThrow(Exception('Failed to fetch data'));
+    when(repository()).thenThrow(Exception());
 
-    expect(() => repository(), throwsException);
+    final result = repository();
+
+    expect(result, throwsA(isA<ApodGenericException>()));
   });
 }
