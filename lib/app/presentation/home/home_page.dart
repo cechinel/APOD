28f4fbcedd82f9b163483b7bbaea6133c84f7237
cechinel <cechine88@gmail.com>
2024-05-11 15:42:21 -0300

    feat: add home page structure

--- lib/app/presentation/home/home_page.dart
+++ lib/app/presentation/home/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:apod/app/presentation/home/widgets/picture_of_the_day_session.dart';
 import 'package:flutter/material.dart';
+
+import 'widgets/pictures_list_session.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,10 +15,41 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Astronomy Picture Of the Day'),
+        backgroundColor: Colors.indigo,
+        title: const Text(
+          'Astronomy Picture Of the Day',
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
+        ),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildPictureOfTheDaySession(),
+          _buildPicturesListSession(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPictureOfTheDaySession() {
+    return const PictureOfTheDaySession(
+      title: 'Picture Of the Day!',
+      pictureTitle: 'Picture title',
+      picture: 'picture url',
+    );
+  }
+
+  Widget _buildPicturesListSession() {
+    return const PicturesListSession(
+      sessionTitle: 'Pictures',
+      picture: 'picture url',
+      pictureTitle: 'picture title',
+      pictureDate: 'picture date',
     );
   }
 }
