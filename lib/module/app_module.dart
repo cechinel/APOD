ff82476769dbfef5d88e05d9008ba61816178a8c
cechinel <cechine88@gmail.com>
2024-05-11 18:15:06 -0300

    chore: inject usecase, repository, datasource and controller to module

--- lib/module/app_module.dart
+++ lib/module/app_module.dart
@@ -1,10 +1,29 @@
+import 'package:apod/app/data/datasources/get_apod_datasource.dart';
+import 'package:apod/app/data/repositories/get_apod_repository_impl.dart';
+import 'package:apod/app/domain/repositories/get_apod_repository.dart';
+import 'package:apod/app/domain/usecases/get_apod_usecase.dart';
+import 'package:apod/app/domain/usecases/get_apod_usecase_impl.dart';
+import 'package:apod/app/external/get_apod_datasource_impl.dart';
+import 'package:apod/app/presentation/home/home_page_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import '../app/presentation/home/home_page.dart';
 
 class AppModule extends Module {
   @override
-  void binds(i) {}
+  void binds(i) {
+    // usecases
+    i.addSingleton<GetApodUsecase>(GetApodUsecaseImpl.new);
+
+    // repositories
+    i.addSingleton<GetApodRepository>(GetApodRepositoryImpl.new);
+
+    // datasources
+    i.addSingleton<GetApodDatasource>(GetApodDatasourceImpl.new);
+
+    // controllers
+    i.addSingleton(HomePageController.new);
+  }
 
   @override
   void routes(RouteManager r) {
