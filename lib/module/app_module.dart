e16bdde3c86ab4604058a5e1a454d388eeb1dbac
cechinel <cechine88@gmail.com>
2024-05-13 13:22:53 -0300

    chore: delete unused shimmer and add DetailsPage in the module

--- lib/module/app_module.dart
+++ lib/module/app_module.dart
@@ -1,5 +1,6 @@
 import 'package:apod/app/data/datasources/get_apod_datasource.dart';
 import 'package:apod/app/data/repositories/get_apod_repository_impl.dart';
+import 'package:apod/app/domain/models/apod_dto.dart';
 import 'package:apod/app/domain/repositories/get_apod_repository.dart';
 import 'package:apod/app/domain/usecases/get_apod_usecase.dart';
 import 'package:apod/app/domain/usecases/get_apod_usecase_impl.dart';
@@ -8,6 +9,7 @@ import 'package:apod/app/presentation/home/home_page_controller.dart';
 import 'package:apod/services/client_https/client_https.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import '../app/presentation/details/details_page.dart';
 import '../app/presentation/home/home_page.dart';
 
 class AppModule extends Module {
@@ -34,7 +36,15 @@ class AppModule extends Module {
     r.add(
       ChildRoute(
         "/",
-        child: (context) => const HomePage(),
+        child: (_) => const HomePage(),
+      ),
+    );
+    r.add(
+      ChildRoute(
+        '/details',
+        child: (_) => DetailsPage(
+          picture: r.args.data as ApodDto,
+        ),
       ),
     );
   }
