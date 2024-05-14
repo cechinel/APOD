f253cd7f8145a0c56e9bbd69a1d851a68d5a0e0d
cechinel <cechine88@gmail.com>
2024-05-13 21:54:42 -0300

    chore: using apiKey from a json file

--- lib/services/client_https/interceptors/api_key_interceptor.dart
+++ lib/services/client_https/interceptors/api_key_interceptor.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 class ApiKeyInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    const apiKey = 'OvUOfeF6SJ16SUxS22yfax9pOYGnOYWNvedwv0IW';
+    const apiKey = String.fromEnvironment('API_KEY');
 
     options.queryParameters.addAll({'api_key': apiKey});
 
